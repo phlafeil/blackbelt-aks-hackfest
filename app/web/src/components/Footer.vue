5bf5ba7f7f0cc2d8c46f1b3b3c588d24405c025c
phlafeil <41090238+phlafeil@users.noreply.github.com>
2018-07-12 16:46:36 -0700

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Chocolate for evryone Team
+        Azure Chocolate for everyone Team
       </div>
       <div class="col-lg-6">
       </div>
